3b2cc5185f801541f71b5c5847a506a52f158f7a
kimbangg <ehehdgus1@hanyang.ac.kr>
2022-01-11 15:02:29 +0900

    2022년 1월 11일 알고리즘 PS

---
+++ 22년 1월/22_01_11/Lv2_짝지어 제어하기.js
@@ -0,0 +1,15 @@
+const solution = s => {
+  let answer = 0;
+  const stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (stack[stack.length - 1] === s[i]) {
+      stack.pop();
+      continue;
+    }
+
+    stack.push(s[i]);
+  }
+
+  return stack.length === 0 ? 1 : 0;
+};
