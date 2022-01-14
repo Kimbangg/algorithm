43a72ca1bf8a12baf1e0ee6ac3e5ca1f681301fd
kimbangg <ehehdgus1@hanyang.ac.kr>
2022-01-14 23:11:56 +0900

    2022년 1월 14일 알고리즘 PS

---
+++ 22년 1월/22_01_14/724_Find Pivot Index.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function (nums) {
+  const sum = nums.reduce((a, b) => a + b, 0);
+  let leftSum = 0;
+  let rightSum = sum;
+  let pastPivotNum = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    rightSum -= nums[i];
+    leftSum += pastPivotNum;
+
+    if (leftSum === rightSum) {
+      return i;
+    }
+
+    pastPivotNum = nums[i];
+  }
+
+  return -1;
+};
