cef5c383f7e0af820e926bcce6f1fba3e9c43632
kimbangg <ehehdgus1@hanyang.ac.kr>
2022-02-04 16:46:11 +0900

    2022년 2월 4일 알고리즘 PS

---
+++ 22년 2월/22_02_04/15649 N과 M (1).js
@@ -0,0 +1,32 @@
+const getPermutation = (cnt, output, permu_arr, visited) => {
+  if (cnt === M) {
+    permu_arr.push(output.map(num => +num));
+    return;
+  }
+
+  for (let i = 1; i <= N; i++) {
+    if (visited[i] === 1) continue;
+
+    visited[i] = 1;
+    output.push(i);
+    getPermutation(cnt + 1, output, permu_arr, visited);
+    output.pop();
+    visited[i] = 0;
+  }
+};
+
+const solution = (N, M, nums) => {
+  const output = [];
+  const permutationResult = [];
+  const visited = Array(N + 1).fill(0);
+
+  getPermutation(0, output, permutationResult, visited);
+
+  return permutationResult;
+};
+
+// 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열을 만들어라.
+const [N, M] = [3, 2];
+const nums = Array.from({ length: N }, (v, i) => i + 1);
+
+console.log(solution(N, M, nums));
